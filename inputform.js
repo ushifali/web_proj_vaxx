1760f3d8f7a76985b5307f614cfcf3e357ce87c8
Shifali U <ushifali65@gmail.com>
2021-11-28 00:16:52 +0530

    input form changes

--- inputform.js
+++ inputform.js
@@ -16,3 +16,19 @@ vaccine_not_taken.onclick = function () {
     option_1.style.display = "none";
     option_2.style.display = "block";
 }
+
+
+//for the second doze
+var vaccine_taken_2 = document.querySelector("#vacc_2_yes")
+var option_3 = document.querySelector("#option_block_3")
+option_3.style.display = "none"
+
+var vaccine_not_taken_2 = document.querySelector("#vacc_2_no")
+
+vaccine_taken_2.onclick = function () {
+option_3.style.display = "block";
+}
+
+ vaccine_not_taken_2.onclick = function () {
+option_3.style.display = "none";
+}
